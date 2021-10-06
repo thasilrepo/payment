61565f1dbab7ee2da91300f87e8ba0c9dd0916f5
Mohamed <mohamth@apac.corpdir.net>
2021-10-07 00:32:24 +0530

    Payment Factory is modified to return Payment object type and also created constants

--- payment-system/src/main/java/com/online/payment/factory/ProductFactory.java
+++ payment-system/src/main/java/com/online/payment/factory/ProductFactory.java
@@ -1,9 +1,25 @@
 package com.online.payment.factory;
 
+import com.online.payment.main.ProductTypeConstants;
+
 public class ProductFactory {
-	
-	public Payment getProductType(String shapeType){
-		
+
+	public Payment getProductType(String productType) {
+
+		if (productType == null) {
+			return null;
+		}
+		if (ProductTypeConstants.PHYSICAL_PRODUCT.equalsIgnoreCase(productType)) {
+			return new PhysicalProduct();
+		} else if (ProductTypeConstants.BOOK.equalsIgnoreCase(productType)) {
+			return new BookPurchase();
+		} else if (ProductTypeConstants.MEMBERSHIP_ACTIVATION.equalsIgnoreCase(productType)) {
+			return new MemberShipActivation();
+		} else if (ProductTypeConstants.MEMBERSHIP_UPGRADE.equalsIgnoreCase(productType)) {
+			return new MemberShipUpgrade();
+		} else if (ProductTypeConstants.VIDEO_PURCHASE.equalsIgnoreCase(productType)) {
+			return new VideoPurchase();
+		}
 		return null;
 	}
 
---
+++ payment-system/src/main/java/com/online/payment/main/ProductTypeConstants.java
@@ -0,0 +1,10 @@
+package com.online.payment.main;
+
+public class ProductTypeConstants {
+	
+	public static final String PHYSICAL_PRODUCT = "PHYSICAL_PRODUCT";
+	public static final String BOOK = "BOOK";
+	public static final String MEMBERSHIP_ACTIVATION = "MEMBERSHIP_ACTIVATION";
+	public static final String MEMBERSHIP_UPGRADE = "MEMBERSHIP_UPGRADE";
+	public static final String VIDEO_PURCHASE = "VIDEO_PURCHASE";
+}
