432e9ba6b586e958e270e6afceda1f05142741c7
Mohamed <mohamth@apac.corpdir.net>
2021-10-07 00:08:40 +0530

    Created class for Membership upgrade

---
+++ payment-system/src/main/java/com/online/payment/factory/MemberShipUpgrade.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.online.payment.factory;
+
+/**
+ * @author Thasil Mohamed
+ *
+ */
+public class MemberShipUpgrade implements Payment {
+
+	/* (non-Javadoc)
+	 * @see com.online.payment.factory.Payment#makePayment(java.lang.String)
+	 */
+	@Override
+	public void makePayment(String paymentType) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
