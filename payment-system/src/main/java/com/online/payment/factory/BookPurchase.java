2e64854739551a90150293ec45b0f52316166d3e
Mohamed <mohamth@apac.corpdir.net>
2021-10-06 23:56:41 +0530

    Created Book purchase class to ship the book

---
+++ payment-system/src/main/java/com/online/payment/factory/BookPurchase.java
@@ -0,0 +1,11 @@
+package com.online.payment.factory;
+
+public class BookPurchase implements Payment {
+
+	@Override
+	public void makePayment(String paymentType) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
