5c3beea183740c58aeac09e38d6b07d1f45e1b24
Mohamed <mohamth@apac.corpdir.net>
2021-10-06 23:22:21 +0530

    This is the initial commit

---
+++ payment-system/src/main/java/com/online/payment/factory/Payment.java
@@ -0,0 +1,19 @@
+/**
+ * This interface provides general payment option
+ */
+package com.online.payment.factory;
+
+/**
+ * @author Thasil Mohamed
+ *
+ */
+public interface Payment {
+	/**
+	 * 
+	 * @param paymentType
+	 */
+	
+	void makePayment(String paymentType);
+	
+
+}
