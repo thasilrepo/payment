e4db6e520ca843a0e0ae92eb08aeb0fa35042306
Mohamed <mohamth@apac.corpdir.net>
2021-10-06 23:41:50 +0530

    Created PackageSlipBean and Physical product classes

---
+++ payment-system/src/main/java/com/online/payment/bean/PackageSlipBean.java
@@ -0,0 +1,116 @@
+package com.online.payment.bean;
+
+import java.util.UUID;
+
+public class PackageSlipBean {
+	
+	private UUID orderNumber;
+	private String firstName;
+	private String lastName;
+	private String address1;
+	private String address2;
+	private String city;
+	private String state;
+	private String mobileNumber;
+	/**
+	 * @return the orderNumber
+	 */
+	public UUID getOrderNumber() {
+		return orderNumber;
+	}
+	/**
+	 * @param orderNumber the orderNumber to set
+	 */
+	public void setOrderNumber(UUID orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the address1
+	 */
+	public String getAddress1() {
+		return address1;
+	}
+	/**
+	 * @param address1 the address1 to set
+	 */
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	/**
+	 * @return the address2
+	 */
+	public String getAddress2() {
+		return address2;
+	}
+	/**
+	 * @param address2 the address2 to set
+	 */
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the mobileNumber
+	 */
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	/**
+	 * @param mobileNumber the mobileNumber to set
+	 */
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
+	
+	
+	
+
+}
---
+++ payment-system/src/main/java/com/online/payment/factory/PhysicalProduct.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.online.payment.factory;
+
+/**
+ * @author MOHAMTH
+ *
+ */
+public class PhysicalProduct implements Payment {
+
+	/* (non-Javadoc)
+	 * @see com.online.payment.factory.Payment#makePayment(java.lang.String)
+	 */
+	@Override
+	public void makePayment(String paymentType) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
